411f56a12a0c26f79d7b8895b99f761770a8b571
etud user <etud@port-dell-precision-7520-22>
2023-01-28 11:08:57 +0100

    update models

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ import datetime
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=220)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -17,14 +17,14 @@ class Question(models.Model):
         ordering='pub_date',
         description='Published recently?',
     )
-    
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=220)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
